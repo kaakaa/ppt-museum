11380756cc8f0471574900b72042736e9c392714
kaakaa <stooner.hoe@gmail.com>
2016-02-22 23:46:56 +0900

    add redis connection pool

--- ppt-museum/src/main/java/org/kaakaa/pptmuseum/db/RedisClient.java
+++ ppt-museum/src/main/java/org/kaakaa/pptmuseum/db/RedisClient.java
@@ -2,18 +2,24 @@ package org.kaakaa.pptmuseum.db;
 
 import org.kaakaa.pptmuseum.db.mongo.DBResourceBundle;
 import redis.clients.jedis.BinaryJedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Created by kaakaa on 16/02/20.
  */
 public class RedisClient {
-    private static final BinaryJedis jedis = new BinaryJedis(DBResourceBundle.getString("redis.host"));
+    private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), DBResourceBundle.getString("redis.host"));
 
     public static void upload(String id, byte[] file) {
-        jedis.set(id.getBytes(), file);
+        try (BinaryJedis bJedis = pool.getResource()) {
+            bJedis.set(id.getBytes(), file);
+        }
     }
 
     public static byte[] get(String id) {
-        return jedis.get(id.getBytes());
+        try (BinaryJedis bJedis = pool.getResource()) {
+            return bJedis.get(id.getBytes());
+        }
     }
 }
---
+++ ppt-museum/src/main/java/org/kaakaa/pptmuseum/db/document/Comment.java
@@ -0,0 +1,7 @@
+package org.kaakaa.pptmuseum.db.document;
+
+/**
+ * Created by kaakaa on 16/02/21.
+ */
+public class Comment {
+}
